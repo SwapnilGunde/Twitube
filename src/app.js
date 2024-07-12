69b73a86238e204fe0a720a42d4755f44e7f8469
Swapnil Gunde <swapnilsanjaygunde1409@gmail.com>
2024-07-12 08:55:22 +0530

    Basic Routing and Controller

--- src/app.js
+++ src/app.js
@@ -14,4 +14,13 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
-export  { app }
+//import routes
+import userRouter from "./routes/user.routes.js"
+
+
+//route declaration 
+app.use("/api/v1/users",userRouter)
+
+//http://localhost:8000/api/v1/user/register
+
+export { app }  
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import app from "./app.js";
+import {app} from "./app.js";
 
 dotenv.config({
   path:'./env'
@@ -8,7 +8,6 @@ dotenv.config({
 
 connectDB()
 .then(()=>{
-  
   app.listen(process.env.PORT || 8000 ,()=>{
     console.log(`Server is listening on port ${process.env.PORT}`)
   })
